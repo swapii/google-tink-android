9d932cd138423ab8ad48e9bd666d21516df2e08d
Pavel Savinov <swapii@gmail.com>
2020-07-17 22:36:07 +0300

    Patch JAR with Javassist and zip4j

--- patcher/build.gradle.kts
+++ patcher/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.utils.addToStdlib.cast
+
 repositories {
     jcenter()
 }
@@ -11,6 +13,14 @@ application {
     mainClass.set("HelloKt")
 }
 
+tasks {
+    withType<JavaExec> {
+        args = project.property("patcher.args").cast<String>().split(' ')
+    }
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation("org.javassist:javassist:3.27.0-GA")
+    implementation("net.lingala.zip4j:zip4j:2.6.1")
 }
